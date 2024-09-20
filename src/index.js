f9e3246e004b764db47b13640a6e290c2c943dd8
yikkai95 <yikkai95@gmail.com>
2024-09-21 04:10:29 +0800

    feat: integrate with angpao substreams

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import 'dotenv/config'
 import {
-    createRequest,
-    streamBlocks,
-    createAuthInterceptor,
-    createRegistry,
-    fetchSubstream
+  createRequest,
+  streamBlocks,
+  createAuthInterceptor,
+  createRegistry,
+  fetchSubstream
 } from '@substreams/core';
 import { createConnectTransport } from "@connectrpc/connect-node";
 import { getCursor } from "./cursor.js";
 import { isErrorRetryable } from "./error.js";
 import { handleResponseMessage } from "./handlers.js"
+import fs from 'fs'
+import { Package } from '@substreams/core/proto';
 
 const TOKEN = process.env.SUBSTREAMS_API_TOKEN
-const ENDPOINT = "https://mainnet.eth.streamingfast.io"
-const SPKG = "https://spkg.io/streamingfast/ethereum-explorer-v0.1.2.spkg"
-const MODULE = "map_block_meta"
-const START_BLOCK = '100000'
-const STOP_BLOCK = '+10000'
+//const ENDPOINT = "https://mainnet.eth.streamingfast.io"
+const ENDPOINT = "https://arbsepolia.substreams.pinax.network:443"
+//const SPKG = "https://spkg.io/streamingfast/ethereum-explorer-v0.1.2.spkg"
+const SPKG = "./angpao.spkg"
+const MODULE = "map_events"
+const START_BLOCK = '82563569'
+const STOP_BLOCK = '+1'
 
 /*
     Entrypoint of the application.
@@ -24,61 +28,63 @@ const STOP_BLOCK = '+10000'
     The application MUST handle disconnections and commit the provided cursor to avoid missing information.
 */
 const main = async () => {
-    const pkg = await fetchPackage()
-    const registry = createRegistry(pkg);
+  const pkg = await fetchPackage()
+  const registry = createRegistry(pkg);
 
-    const transport = createConnectTransport({
-        baseUrl: ENDPOINT,
-        interceptors: [createAuthInterceptor(TOKEN)],
-        useBinaryFormat: true,
-        jsonOptions: {
-            typeRegistry: registry,
-        },
-    });
-    
-    // The infite loop handles disconnections. Every time an disconnection error is thrown, the loop will automatically reconnect
-    // and start consuming from the latest commited cursor.
-    while (true) {
-        try {
-            await stream(pkg, registry, transport);
+  const transport = createConnectTransport({
+    baseUrl: ENDPOINT,
+    interceptors: [createAuthInterceptor(TOKEN)],
+    useBinaryFormat: true,
+    jsonOptions: {
+      typeRegistry: registry,
+    },
+  });
 
-            // Break out of the loop when the stream is finished
-            break;
-        } catch (e) {
-            if (!isErrorRetryable(e)) {
-              console.log(`A fatal error occurred: ${e}`)
-              throw e
-            }
-            console.log(`A retryable error occurred (${e}), retrying after backoff`)
-            console.log(e)
-            // Add backoff from a an easy to use library
-        }
+  // The infite loop handles disconnections. Every time an disconnection error is thrown, the loop will automatically reconnect
+  // and start consuming from the latest commited cursor.
+  while (true) {
+    try {
+      await stream(pkg, registry, transport);
+
+      // Break out of the loop when the stream is finished
+      break;
+    } catch (e) {
+      if (!isErrorRetryable(e)) {
+        console.log(`A fatal error occurred: ${e}`)
+        throw e
+      }
+      console.log(`A retryable error occurred (${e}), retrying after backoff`)
+      console.log(e)
+      // Add backoff from a an easy to use library
     }
+  }
 }
 
 const fetchPackage = async () => {
-    return await fetchSubstream(SPKG)
+  const b = fs.readFileSync(SPKG)
+  return Package.fromBinary(b)
+  //return await fetchSubstream(SPKG)
 }
 
 const stream = async (pkg, registry, transport) => {
-    const request = createRequest({
-        substreamPackage: pkg,
-        outputModule: MODULE,
-        productionMode: true,
-        startBlockNum: Number(START_BLOCK),
-        stopBlockNum: STOP_BLOCK,
-        startCursor: await getCursor() ?? undefined
-    });
-    
-    // Stream the blocks
-    for await (const response of streamBlocks(transport, request)) {
-        /*
-            Decode the response and handle the message.
-            There different types of response messages that you can receive. You can read more about the response message in the docs:
-            https://substreams.streamingfast.io/documentation/consume/reliability-guarantees#the-response-format
-        */
-        await handleResponseMessage(response.message, registry);
-    }
+  const request = createRequest({
+    substreamPackage: pkg,
+    outputModule: MODULE,
+    productionMode: false,
+    startBlockNum: Number(START_BLOCK),
+    stopBlockNum: STOP_BLOCK,
+    startCursor: await getCursor() ?? undefined
+  });
+
+  // Stream the blocks
+  for await (const response of streamBlocks(transport, request)) {
+    /*
+        Decode the response and handle the message.
+        There different types of response messages that you can receive. You can read more about the response message in the docs:
+        https://substreams.streamingfast.io/documentation/consume/reliability-guarantees#the-response-format
+    */
+    await handleResponseMessage(response.message, registry);
+  }
 }
 
 main()
