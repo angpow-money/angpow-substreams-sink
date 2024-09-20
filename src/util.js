2381c9f33306d5335af21964a9442ca9ab744873
yikkai95 <yikkai95@gmail.com>
2024-09-21 03:48:35 +0800

    feat: copy from template

---
+++ src/util.js
@@ -0,0 +1,10 @@
+export const objectToJsonString = obj => {
+  /*
+      Workaround because JS built-in function is not able to convert BigInt
+      https://github.com/GoogleChromeLabs/jsbi/issues/30
+  */
+  ///@ts-ignore
+  BigInt.prototype.toJSON = function() { return this.toString() }
+
+  return JSON.stringify(obj)
+}
